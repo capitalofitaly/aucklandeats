e6b9df2fea076827120ce411a34f9c166991b35a
capitalofitaly <lising.r@maristcollege.school.com>
2022-08-29 12:50:07 +1200

    added nachos.html and pork.html to top3. added the rest of the top3 images into the image file so I can access it

--- script.js
+++ script.js
@@ -1,4 +1,47 @@
-let result = confirm ("Take this quiz so we can suggest a place to eat!")
+const start = document.getElementById("start");
+const quiz = document.getElementById("quiz");
+const question = document.getElementById("question");
+const qImg = document.getElementById("qImg");
+const choiceA = document.getElementById("A");
+const choiceB = document.getElementById("B");
+const choiceC = document.getElementById("C");
+const answer = document.getElementById("answer");
 
-console.log (result);
+let question = [
+    {
+        question: "What price range are you looking for?",
+        choiceA: "$",
+        choiceB: "$$",
+        choiceC: "$$$",
+    }
+    {
+        question: "Sweet or Savoury?",
+        choiceA: "Sweet",
+        choiceB: "Savoury",
+        choiceC: "Not too sure",  
+    }
+    {
+        question: "Would you like something filling? Or something small?",
+        choiceA: "I just want a snack",
+        choiceB: "I'm quite hungry",
+        choiceC: "I'm very hungry",
+    }
+    {
+        question: "What kind of cuisine are you looking for?",
+        choiceA: "Asian",
+        choiceB: "Western",
+        choiceC: "Fusion",
+    }
+];
 
+const lastQuestion = questions.length - 1;
+let runningQuestion = 0;
+
+function renderQuestion(){
+    let q = questions[runningQuestion];
+
+    question.innerHTML = "<p>+ q.question +</p>";
+    choiceA.innerHTML = q.choiceA;
+    choiceB.innerHTML = q.choiceB;
+    choiceC.innerHTML = q.choiceC;
+}
