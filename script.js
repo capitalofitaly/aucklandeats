41f65071f640f639f6e82a81f044cdde785858f7
capitalofitaly <lising.r@maristcollege.school.com>
2022-09-13 10:54:32 +1200

    did the css, html and started js neccessary for the quiz.

--- script.js
+++ script.js
@@ -1,48 +1,10 @@
-const start = document.getElementById("start");
-const quiz = document.getElementById("quiz");
-const question = document.getElementById("question");
-const qImg = document.getElementById("qImg");
-const choiceA = document.getElementById("A");
-const choiceB = document.getElementById("B");
-const choiceC = document.getElementById("C");
-const answer = document.getElementById("answer");
+var questions = $('.question').length;
+var total  = 0;
+var average = 0;
 
-let questions = [
-    {
-        question: "What price range are you looking for?",
-        choiceA: "$",
-        choiceB: "$$",
-        choiceC: "$$$",
-    }, {
-        question: "Sweet or Savoury?" ,
-        choiceA: "Sweet" ,
-        choiceB: "Savoury",
-        choiceC: "Not too sure",  
-    }, {
-        question: "Would you like something filling? Or something small?",
-        choiceA: "I just want a snack",
-        choiceB: "I'm quite hungry",
-        choiceC: "I'm very hungry",
-    }, {
-        question: "What kind of cuisine are you looking for?",
-        choiceA: "Asian",
-        choiceB: "Western",
-        choiceC: "Fusion",
-    }
-];
+$('.answer').on('click' , function(){
+    $9(this).addClass('selected');
+    total += $(this).data('value');
 
-const lastQuestion = questions.length - 1;
-let runningQuestion = 0;
-
-function renderQuestion(){
-    let q = questions[runningQuestion];
-
-    question.innerHTML = "<p>"+ q.question +"</p>";
-    choiceA.innerHTML = q.choiceA;
-    choiceB.innerHTML = q.choiceB;
-    choiceC.innerHTML = q.choiceC;
-}
-
-start.style.display = "none"; 
-renderQuestion(); 
-quiz.style.display = "block"; 
+    console.log(total);
+})
